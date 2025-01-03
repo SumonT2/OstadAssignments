5aaa629c1a4684a4b370a7a5dd45dd7f924e2820
SumonT2 <sumon@techsflow.com>
2025-01-03 23:04:31 +0600

    implement all code

---
+++ lib/e.dart
@@ -0,0 +1,21 @@
+import 'c.dart';
+import 'd.dart';
+
+void main() {
+
+  var student = Student("Sumon K. Mondol", 31, "Vatara, Dhaka", "1011123", "A", [90, 85, 82,100]);
+  print("Student Information:\n");
+  student.displayRole();
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Address: ${student.address}");
+  print("Average Score: ${student.calculateAverageScore().toStringAsFixed(1)}\n");
+
+  var teacher = Teacher("Toufiq", 35, "Gulshan, Dhaka", "T456123", ["Java", "Python", "Dart"]);
+  print("Teacher Information:\n");
+  teacher.displayRole();
+  print("Name: ${teacher.name}");
+  print("Age: ${teacher.age}");
+  print("Address: ${teacher.address}");
+  teacher.displayCoursesTaught();
+}
